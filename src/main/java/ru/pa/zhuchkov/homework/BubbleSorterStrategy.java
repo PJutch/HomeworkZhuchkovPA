2e311f0e657b8ff12492e7e8945d625d158daee5
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-12 20:18:20 +0300

    Add limit for List size and tests for failing strategies

--- src/main/java/ru/pa/zhuchkov/homework/BubbleSorterStrategy.java
+++ src/main/java/ru/pa/zhuchkov/homework/BubbleSorterStrategy.java
@@ -1,11 +1,16 @@
 package ru.pa.zhuchkov.homework;
 
+import javax.naming.SizeLimitExceededException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BubbleSorterStrategy implements Sorter.Strategy {
     @Override
-    public List<Integer> sort(List<Integer> list) {
+    public List<Integer> sort(List<Integer> list) throws IllegalArgumentException {
+        if (list.size() > 10000) {
+            throw new IllegalArgumentException("List is to big to be sorted with an O(n^2) bubble sort");
+        }
+
         List<Integer> result = new ArrayList<>(list);
         for (int iteration = 0; iteration < result.size(); ++iteration) {
             for (int i = 0; i + 1 < result.size(); ++i) {
--- src/test/java/ru/pa/zhuchkov/homework/SorterTests.java
+++ src/test/java/ru/pa/zhuchkov/homework/SorterTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +34,42 @@ public class SorterTests {
         final Sorter sorter = new Sorter();
         assertThrows(IllegalStateException.class, () -> {
             sorter.sort(List.of(3, 2, 1));
+        });
+    }
+
+    @Test
+    void fallbackSort() {
+        final Sorter sorter = new Sorter();
+        sorter.addStrategy(new BubbleSorterStrategy());
+        sorter.addStrategy(new BuiltinSorterStrategy());
+
+        final int LIST_SIZE = 11000;
+        List<Integer> sorted = new ArrayList<>(LIST_SIZE);
+        for (int i = 0; i < LIST_SIZE; i++) {
+            sorted.add(i);
+        }
+
+        List<Integer> reversed = new ArrayList<>(LIST_SIZE);
+        for (int i = LIST_SIZE - 1; i >= 0; i--) {
+            reversed.add(i);
+        }
+
+        assertEquals(sorted, sorter.sort(reversed));
+    }
+
+    @Test
+    void allSortsFailed() {
+        final Sorter sorter = new Sorter();
+        sorter.addStrategy(new BubbleSorterStrategy());
+
+        final int LIST_SIZE = 11000;
+        List<Integer> list = new ArrayList<>(LIST_SIZE);
+        for (int i = LIST_SIZE - 1; i >= 0; i--) {
+            list.add(i);
+        }
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sorter.sort(list);
         });
     }
 }
