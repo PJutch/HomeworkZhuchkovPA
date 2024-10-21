cdeb04ad42e8042c65bf9be06f8c4f73232037f4
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-21 10:39:50 +0300

    Refactor Message to be a record

--- src/main/java/ru/pa/zhuchkov/homework/Message.java
+++ src/main/java/ru/pa/zhuchkov/homework/Message.java
@@ -2,33 +2,8 @@ package ru.pa.zhuchkov.homework;
 
 import java.util.Map;
 
-public class Message {
-    private final Map<String, String> content;
-    private final EnrichmentType enrichmentType;
-
+public record Message(Map<String, String> content, EnrichmentType enrichmentType) {
     public enum EnrichmentType {
         MSISDN
-    }
-
-    public Message(Map<String, String> content, EnrichmentType enrichmentType) {
-        this.content = content;
-        this.enrichmentType = enrichmentType;
-    }
-
-    public Message(Message message) {
-        content = message.content;
-        enrichmentType = message.enrichmentType;
-    }
-
-    public String get(String key) {
-        return content.get(key);
-    }
-
-    public void put(String key, String value) {
-        content.put(key, value);
-    }
-
-    public EnrichmentType enrichmentType() {
-        return enrichmentType;
     }
 }
