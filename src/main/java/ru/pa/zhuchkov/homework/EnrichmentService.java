a959ef5207091000a781eee548ca72d0208811a6
PJutch <103033969+PJutch@users.noreply.github.com>
2024-10-21 11:02:08 +0300

    Implement EnrichmentService

--- src/main/java/ru/pa/zhuchkov/homework/EnrichmentService.java
+++ src/main/java/ru/pa/zhuchkov/homework/EnrichmentService.java
@@ -1,7 +1,26 @@
 package ru.pa.zhuchkov.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnrichmentService {
+    private final HashMap<Message.EnrichmentType, Enrichment> enrichments = new HashMap<>();
+
+    public interface Enrichment {
+        Map<String, String> enrich(Map<String, String> input);
+    }
+
+    public void addEnrichment(Message.EnrichmentType type, Enrichment enrichment) {
+        enrichments.put(type, enrichment);
+    }
+
     public Message enrich(Message message) {
-        return null;
+        Enrichment enrichment = enrichments.get(message.enrichmentType());
+        if (enrichment == null) {
+            return message;
+        }
+
+        Map<String, String> newContent = enrichment.enrich(new HashMap<>(message.content()));
+        return new Message(newContent, message.enrichmentType());
     }
 }
